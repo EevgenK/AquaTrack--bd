e05c48d956fc35aa8deef2740a53a8479f151016
Evgen Kulbachenko <45nalitay45@gmail.com>
2025-03-05 15:20:19 +0200

    user schema is done

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -13,10 +13,47 @@ const usersSchema = new Schema(
       enum: validationParams.gender,
       default: 'woman',
     },
-    // weight: {
-    //   type: Number,
-    //   /*TO GO ON FROM HERE*/
-    // },
+    weight: {
+      value: {
+        type: Number,
+        default: 0,
+        min: 0,
+      },
+      unit: {
+        type: String,
+        enum: ['kg'],
+        default: 'kg',
+      },
+    },
+    dailySportTime: {
+      value: {
+        type: Number,
+        default: 0,
+        min: 0,
+      },
+      unit: {
+        type: String,
+        enum: ['hours'],
+        default: 'hours',
+      },
+    },
+    dailyWaterNorm: {
+      value: {
+        type: Number,
+        default: 1500,
+        min: 0,
+      },
+      unit: {
+        type: String,
+        enum: ['ml'],
+        default: 'ml',
+      },
+    },
+    avatar: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/dobkaq79k/image/upload/v1738604656/ogeqhuxc…' /*Temporary*/,
+    },
   },
   {
     timestamps: true,
