82c3957a636ba7709292bdc8968500256f540991
Alex <zemzikova.oleksandra@gmail.com>
2025-03-06 06:34:13 +0200

    add authorization Google, I can't check because I don't have any environment variables

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,13 +1,28 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { registerUserSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
+import {
+  loginWithGoogleOAuthSchema,
+  registerUserSchema,
+} from '../validation/auth.js';
+import {
+  getGoogleOAuthUrlController,
+  loginWithGoogleController,
+  registerUserController,
+} from '../controllers/auth.js';
 const router = Router();
 router.post(
   '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
+);
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
 );
 
 export default router;
