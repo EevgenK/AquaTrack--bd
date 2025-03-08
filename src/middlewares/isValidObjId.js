db9bf39d0b40e0fc85f23e60c746e56b1cc50aae
svynoslav <badbloodprince@gmail.com>
2025-03-08 12:05:51 +0200

    date validation, excessive if
    Updated water date validation to validate "2025-03-07T15:30" format
    
    Removed excessive if

--- src/middlewares/isValidObjId.js
+++ src/middlewares/isValidObjId.js
@@ -4,10 +4,6 @@ import createHttpError from 'http-errors';
 export const isValidObjId = (req, res, next) => {
   const { id } = req.params;
 
-  if (!id) {
-    return next(createHttpError(400, 'Missing id in isValidObjId'));
-  }
-
   if (!isValidObjectId(id)) {
     return next(createHttpError(400, 'Invalid id format'));
   }
--- src/validation/validationRegEx.js
+++ src/validation/validationRegEx.js
@@ -1,1 +1,1 @@
-export const dateRegEx = /^\d{2}-\d{2}-\d{2}$/;
+export const dateRegEx = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
